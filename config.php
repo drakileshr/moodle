35a235488054e2d96cf3d591b8cfbea7bc07eb28
drakileshr <akident@gmail.com>
2021-08-12 20:09:40 +0530

    Update and rename config2.php to config.php

--- config.php
+++ config.php
@@ -1,16 +1,22 @@
 <?php  // Moodle configuration file
-
 unset($CFG);
 global $CFG;
 $CFG = new stdClass();
+$dbopts = parse_url(getenv('DATABASE_URL'));
 
-$CFG->dbtype    = getenv('DATABASE_TYPE');
+//$CFG->dbhost    = getenv('DATABASE_HOST');
+//$CFG->dbname    = getenv('DATABASE_NAME');
+//$CFG->dbuser    = getenv('DATABASE_USER');
+//$CFG->dbpass    = getenv('DATABASE_PASSWORD');
+//$CFG->prefix    = 'mdl_';
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$CFG->dbtype    = 'pgsql';
 $CFG->dblibrary = 'native';
-$CFG->dbhost    = getenv('DATABASE_HOST');
-$CFG->dbname    = getenv('DATABASE_NAME');
-$CFG->dbuser    = getenv('DATABASE_USER');
-$CFG->dbpass    = getenv('DATABASE_PASSWORD');
+$CFG->dbhost    = $dbopts["host"];
+$CFG->dbuser    = $dbopts["user"];
+$CFG->dbpass    = $dbopts["pass"];
 $CFG->prefix    = 'mdl_';
+
 $CFG->dboptions = array (
   'dbpersist' => 0,
   'dbport' => getenv('DATABASE_PORT'),
